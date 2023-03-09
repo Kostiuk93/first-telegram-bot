bb619049e78cddff9222fcdd91a342ebce547ab2
Kostiuk93 <kostiuk93@gmail.com>
2023-03-09 12:43:51 +0300

    init 3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="App">
-        <button onClose={onClose}>Закрыть</button>
+        <button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
