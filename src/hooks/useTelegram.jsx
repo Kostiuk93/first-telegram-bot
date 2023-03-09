719608efec417d9cc779be152edc18b19f77a45a
Kostiuk93 <kostiuk93@gmail.com>
2023-03-09 20:29:32 +0300

    init

--- src/hooks/useTelegram.jsx
+++ src/hooks/useTelegram.jsx
@@ -14,6 +14,7 @@ export function useTelegram() {
         onClose,
         onToggleButton,
         tg,
-        user: tg.initDataUnsafe?.user
+        user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id
     }
 }
